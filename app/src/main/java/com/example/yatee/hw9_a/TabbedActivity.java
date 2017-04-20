400062ca7f123393d53996e54cbd029a3995a3de
Yateen Kedare <ykedare@uncc.edu>
2017-04-19 22:18:25 -0400

    fixed profile progressDialog

--- app/src/main/java/com/example/yatee/hw9_a/TabbedActivity.java
+++ app/src/main/java/com/example/yatee/hw9_a/TabbedActivity.java
@@ -43,7 +43,7 @@ public class TabbedActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setOffscreenPageLimit(0);
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
