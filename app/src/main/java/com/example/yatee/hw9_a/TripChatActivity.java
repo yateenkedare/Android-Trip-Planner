0026af3a8831f18e5e1e8ef4141226ad0ed5e198
Yateen Kedare <ykedare@uncc.edu>
2017-04-21 02:50:17 -0400

    Adding Trips

---
+++ app/src/main/java/com/example/yatee/hw9_a/TripChatActivity.java
@@ -0,0 +1,26 @@
+package com.example.yatee.hw9_a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TripChatActivity extends AppCompatActivity {
+    //TODO add Friends from Friends List
+    //TODO add Trip Details
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_chat);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
